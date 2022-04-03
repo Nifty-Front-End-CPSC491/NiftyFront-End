aa82fa174fc590a61f8995a6384f6b6d8b2ee1a5
yp1220 <79236656+yp1220@users.noreply.github.com>
2022-04-03 16:12:04 -0700

    Created Meta Mask pop-up window

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ window.userWalletAddress = null
 const loginButton = document.getElementById("buttonMeta")
 const userWallet = document.getElementById('userWallet')
 
+
+document.getElementById('person').addEventListener('click', function()
+{
+    document.getElementById('pop-up').style.display = "flex"
+});
+
+document.getElementById('close').addEventListener('click', function()
+{
+    document.getElementById('pop-up').style.display = "none"
+});
+
+
+
+
+
 function toggleButton() {
     if (!window.ethereum) {
         loginButton.innerText = 'MetaMask is not installed'
@@ -44,3 +59,31 @@ function signOutOfMetaMask() {
 window.addEventListener('DOMContentLoaded', () => {
     toggleButton()
 });
+
+// Slideshow
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demodots");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
